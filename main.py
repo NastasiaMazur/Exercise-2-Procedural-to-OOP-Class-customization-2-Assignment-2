366e0c3d6b4334ada2713c47e2c8c106b9875cf6
NastasiaMazur <nvmazour@mail.ru>
2023-12-09 23:23:37 +0100

    Assignment 2 Phase2

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-Phase 1
+#Phase 1
 class Point:
     def __init__(self, x: float, y: float):
         self.x = x
@@ -17,3 +17,20 @@ p3 = Point(12.2, 28.7)
 
 print(p1)
 print([p1, p2, p3])
+
+#Phase 2
+class Shape(list[Point,...]):   #a shape class inherits from a list of points --> it has all the properties list of points has
+    def __init__(self, *points):            # this would be inheritance
+        self.points = list(points)
+
+    def __str__(self):
+        return f"Shape {self.points}"
+
+# Example usage:
+s1 = Shape(p1, p2, p3)
+s2 = Shape(p2)
+s3 = Shape()
+
+print(s1)
+print(s2)
+print(s3)
